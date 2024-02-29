b3910d1f4952d4981d619f010f40eb3944ff5f2b
ronelsolomon <rsolomon1@dons.usfca.edu>
2024-02-29 15:16:09 -0800

    new

--- news_data_call.py
+++ news_data_call.py
@@ -4,6 +4,10 @@ import json
 import requests
 import os
 import requests
+import requests
+import re
+import json
+from bs4 import BeautifulSoup
 
 
 
@@ -35,4 +39,65 @@ def wrtie_json_to_gcs(bucket_name, blob_name, service_account_key_file, data):
     bucket = storage_client.bucket(bucket_name)
     blob = bucket.blob(blob_name)
     with blob.open("w") as f:
-        json.dump(data, f)    
+        json.dump(data, f)
+        
+
+
+
+
+def scrape_cnn_articles():
+    # URL
+    url = 'https://www.cnn.com'
+
+    # Send an HTTP GET request to the URL
+    response = requests.get(url)
+
+    # Parse the HTML content of the response using BeautifulSoup
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    # Find all links on the page
+    links = [link.get('href') for link in soup.find_all('a')]
+
+    # Filter URLs to those starting with '/2024'
+    filtered_urls = ["https://www.cnn.com" + url for url in links if url and url.startswith('/20')]
+
+    articles = []
+
+    for link in filtered_urls:
+        
+        r = requests.get(link)
+
+        # Extract date from the URL
+        date_pattern = r'/(\d{4}/\d{2}/\d{2})/'
+        date_match = re.search(date_pattern, link)
+        date = date_match.group(1) if date_match else None
+
+        # Extract HTML content
+        html_content = r.content.decode('utf-8')
+
+        # Extract article body
+        body_pattern = r'"articleBody"\s*:\s*"([^"]+)"'
+        body_match = re.search(body_pattern, html_content)
+        body = body_match.group(1) if body_match else None
+
+        # Extract article headline
+        header_pattern = r'"headline"\s*:\s*"([^"]+)"'
+        header_match = re.search(header_pattern, html_content)
+        header = header_match.group(1) if header_match else None
+
+        # Extract article author
+        author_pattern = r'"author"\s*:\s*"([^"]+)"'
+        author_match = re.search(author_pattern, html_content)
+        author = author_match.group(1) if author_match else None
+
+        article = {
+            "URL": link,
+            "Date": date,
+            "Header": header,
+            "Author": author,
+            "Text": body
+        }
+
+        articles.append(article)
+    return articles
+

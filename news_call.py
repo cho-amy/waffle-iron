d3bf1afd8e0d4cbd640e5dd4c9528b2a6c26a7cd
cho-amy <amycho99@gmail.com>
2024-03-01 13:25:54 -0800

    fixing airflow file

--- news_call.py
+++ news_call.py
@@ -12,31 +12,20 @@ from datetime import datetime
 
 def load_data():
     for url in news.keys():
-        data = retreive_api_data(url,api,pages)
+        data = retreive_api_data(url, api, pages)
+        data = preprocess(data)
         blob = f"{today}/{news[url]}.json"
-        write_json_to_gcs(bucket_name,blob,service_account_key_file,data)
-
-
-def preprocess(data):
-    findata = []
-    for page in data:
-        for news_item in page['news']:
-            if 'date' in news_item:
-                news_item['date'] = datetime.strptime(news_item['date'], '%a, %d %b %Y %H:%M:%S %Z').strftime('%Y-%m-%d')
-            findata.append(news_item)
-    return findata
+        write_json_to_gcs(bucket_name, blob, service_account_key_file, data)
 
 
 with DAG(
     dag_id="waffle",
-    schedule= "@daily",
+    schedule="@daily",
     start_date=datetime(2024, 2, 28),
     catchup=False
 ) as dag:
-    API = PythonOperator(task_id = "APi_call",
-                                                  python_callable = load_data,
-                                                  dag=dag)
-
+    API = PythonOperator(task_id="APi_call",
+                         python_callable=load_data,
+                         dag=dag)
 
     API
-
